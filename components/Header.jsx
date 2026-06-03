1346fc41477f7dcd254e49fe988b9bb351df9907
jolly vivek <vivekkumar.rwit@gmail.com>
2026-06-03 12:20:26 +0530

    Add comapny name and experience in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
             <Image src={assets.hand_icon} className='w-6' alt=''/> 
         </h3>
         <h1 className='text-3xl sm:text-5xl lg:text-[66px]'>Frontend ReactJs Developer and MERN Stack Developer.</h1>
-        <p className='max-w-2xl mx-auto'>I am a frontend developer from Ambala, Haryana with 2.3 years of experience in React Js and 1+ year in Mern stack 
-        companies like RW Infotech, Bfox Pvt Lmt.</p>
+        <p className='max-w-2xl mx-auto'>I am a frontend developer from Ambala, Haryana with 2.3 years of experience in React Js and 1 + year in Mern stack 
+        companies like RW Infotech, Bfox Pvt Lmt. and Eways pvt Lmt.</p>
         <div className='flex flex-col sm:flex-row justify-center items-center gap-4 mt-4'>
           <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>Contact Me <Image src={assets.right_arrow_white} className=' w-4' alt=''/>  </a>
           <a href="/VivekCVMERN.pdf" target='_blank' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
